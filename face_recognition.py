17429a73c58d8bcbf361f1d2642c551ea68013e6
Utsav <utsav2005maji@gmail.com>
2024-02-01 13:00:57 +0530

    finalized

--- face_recognition.py
+++ face_recognition.py
@@ -1,29 +1,46 @@
+# Import necessary libraries
 import numpy as np
 import cv2 as cv
 
+# Load the Haar Cascade for face detection
 haar_cascade = cv.CascadeClassifier('E:\Documents\Python\Open CV\FaceDetection\HardCascades\haar_face.xml')
 
-people = ['Udita','John Wick']
-# features = np.load('features.npy')
-# labels = np.load('labels.npy')
+# Define a list of people for recognition
+people = ['Udita', 'John Wick']
 
+# Load the pre-trained features and labels
+features = np.load('features.npy', allow_pickle=True)
+labels = np.load('labels.npy', allow_pickle=True)
+
+# Create an LBPH face recognizer
 face_recognizer = cv.face.LBPHFaceRecognizer_create()
+
+# Read the pre-trained face recognizer model
 face_recognizer.read('face_trained.yml')
 
-img = cv.imread(r'Train\Udita\photo_2024-01-31_19-51-29 (2).jpg')
-gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-cv.imshow('Person',gray)
+# Read an image for testing
+img = cv.imread(r'test2.JPG')
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-# detect the face in the image
-faces_rect = haar_cascade.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=3)
-for x,y,w,h in faces_rect:
-    faces_roi = gray[y:y+w,x:x+h]
+# Detect faces in the grayscale image
+faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3)
 
-    label,confidence = face_recognizer.predict(faces_roi)
+# Iterate over each detected face and perform face recognition
+for x, y, w, h in faces_rect:
+    faces_roi = gray[y:y+w, x:x+h]
+
+    # Predict the label and confidence for the detected face
+    label, confidence = face_recognizer.predict(faces_roi)
+
+    # Print the predicted label and confidence
     print(f'label = {people[label]} with a confidence of {confidence}')
 
-    cv.putText(img,str(people[label]),(20,20),cv.FONT_HERSHEY_PLAIN,1.0,(0,255,0),thickness=1)
-    cv.rectangle(img,(x,y),(x+w,y+h),(0,255,0),thickness=2)
+    # Display the recognized label on the image
+    cv.putText(img, str(people[label]), (20, 20), cv.FONT_HERSHEY_PLAIN, 1.0, (0, 255, 0), thickness=1)
 
-cv.imshow('Detected Person',img)
+    # Draw a rectangle around the detected face
+    cv.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), thickness=2)
+
+# Display the image with detected faces
+cv.imshow('Detected Person', img)
 cv.waitKey(0)
--- face_train.py
+++ face_train.py
@@ -1,46 +1,65 @@
+# Import necessary libraries
 import os
 import cv2 as cv
 import numpy as np
 
+# Define a list of people to recognize
 people = ['Udita','John Wick']
+
+# Specify the directory where the training data is located
 DIR = r'E:\Documents\Python\Open CV\Face Recognition\Train'
 
+# Load the Haar Cascade for face detection
 haar_cascade = cv.CascadeClassifier('E:\Documents\Python\Open CV\FaceDetection\HardCascades\haar_face.xml')
 
+# Initialize empty lists to store features and labels for training
 features = []
 labels = []
 
+# Function to create training data
 def create_train():
     for person in people:
-        path = os.path.join(DIR,person)
+        # Construct the path for each person's images
+        path = os.path.join(DIR, person)
+        # Assign a unique label to each person
         label = people.index(person)
 
+        # Loop through each image in the person's directory
         for img in os.listdir(path):
-            img_path = os.path.join(path,img)
+            # Construct the full path for the image
+            img_path = os.path.join(path, img)
 
+            # Read the image and convert it to grayscale
             img_array = cv.imread(img_path)
-            gray = cv.cvtColor(img_array,cv.COLOR_BGR2GRAY)
+            gray = cv.cvtColor(img_array, cv.COLOR_BGR2GRAY)
 
-            faces_rect =  haar_cascade.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=3)
+            # Detect faces in the grayscale image
+            faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3)
 
-            for x,y,w,h in faces_rect:
-                faces_roi = gray[y:y+h,x:x+w]
+            # Iterate over each detected face and extract the region of interest
+            for x, y, w, h in faces_rect:
+                faces_roi = gray[y:y+h, x:x+w]
+                # Append the face region to the features list and the corresponding label to the labels list
                 features.append(faces_roi)
                 labels.append(label)
 
+# Call the function to create the training data
 create_train()
 print('Training Done---------')
 
-features = np.array(features,dtype='object')
+# Convert features list to NumPy array with 'object' datatype
+features = np.array(features, dtype='object')
 labels = np.array(labels)
 
+# Create an LBPH face recognizer
 face_recognizer = cv.face.LBPHFaceRecognizer_create()
 
-# Train the recognizer on the features nd labels list
+# Train the recognizer using the features and labels
+face_recognizer.train(features, labels)
 
-face_recognizer.train(features,labels)
-
+# Save the trained face recognizer model to a file
 face_recognizer.save('face_trained.yml')
 
-np.save('features.npy',features)
-np.save('labels.npy',labels)
+# Save the features and labels to NumPy arrays
+np.save('features.npy', features)
+np.save('labels.npy', labels)
